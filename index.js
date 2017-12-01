b7e078b900e160a08e02fe54b4b9876c4e95a80c
Tero Keski-Valkama <tero.keski-valkama@cybercom.com>
2017-12-02 00:28:47 +0200

    Added a server for testing and initial content.

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3000);
